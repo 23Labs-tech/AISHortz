7d46c42975d34c447a3cfdad2a055e4cc6e48ce1
next-gen-developer-bit <sociallabs101@gmail.com>
2025-12-22 18:00:52 +0530

    Fix ResetPassword import path

--- components/ResetPassword.tsx
+++ components/ResetPassword.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import AuthButton from "./../components/AuthButton";
+import AuthButton from "./AuthButton";
 // import { useRouter, useSearchParams } from "next/navigation";
 
 const ResetPassword = () => {
